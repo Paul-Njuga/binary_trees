c5777c81e596fbef908ef7c108cc1156ca851e3e
Paul Njuga <paulnjuga@gmail.com>
2023-03-28 23:55:08 +0300

    Malloc node instead of parent

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -5,19 +5,20 @@
  * @parent: pointer to the parent node of the node to create
  * @value: value to put in the new node
  *
- * Return: pointer to the new node, or NULL on failure
+ * Return: pointer to the new node
+ * 	   NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *tempNode = malloc(sizeof(parent));
+	binary_tree_t *new;
 
-	if (!tempNode)
+	new = malloc(sizeof(binary_tree_t));
+	if (!new)
 		return (NULL);
 
-	tempNode->n = value;
-	tempNode->parent = parent;
-	tempNode->left = NULL;
-	tempNode->right = NULL;
-
-	return (tempNode);
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
+	return (new);
 }
