3a9fdfc3a1167d96cc258b4b38db7f1fb00bd2fb
Paul Njuga <paulnjuga@gmail.com>
2023-03-30 23:28:08 +0300

    Edit for betty

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,8 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - function that counts the nodes with at least 1 child in a binary tree
- * @tree: Pointer to the root node of the tree to count the number of nodes
+ * binary_tree_nodes - function that counts the nodes,
+ * with at least 1 child in a binary tree
+ * @tree: Pointer to the root node of the tree,
+ * to count the number of nodes
  *
  * Return: Number of nodes
  */
@@ -15,7 +17,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	else if (!tree->left || !tree->right)
 		return (1);
 
-	nodes = binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1;
+	nodes = (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right) + 1);
 
 	return (nodes);
 }
